7041426ef7f66b799892f2017ad0a274c361f2f8
dmgarifullin <dmgarifullin@sberautotech.ru>
2026-01-25 15:07:47 +0300

    fix startup app

--- api/main.py
+++ api/main.py
@@ -26,7 +26,7 @@ if __name__ == "__main__":
     import uvicorn
 
     uvicorn.run(
-        "app.main:app",
+        "api.main:app",
         host="0.0.0.0",
         port=int(settings.http_port),
         reload=True,
